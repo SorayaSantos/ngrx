8cb3e8048a4b1f753376f515cb1aeb64a345f188
SorayaSantos <soraya.ss_3@hotmail.com>
2021-01-31 17:03:36 +0000

    267

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Person } from './person';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import * as faker from 'faker';
+import { AppState } from './store';
+import { select, Store } from '@ngrx/store';
+import { personNew } from './store/person.actions';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +13,26 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
 
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    this.people$ = this.store.pipe(select('people'));
+    console.log(this.people$)
+  }
+
   people$: Observable<Person[]>;
 
   addNew() {
+    let person: Person = {
+      name: faker.name.findName(),
+      address: faker.address.streetAddress(),
+      city: faker.address.city(),
+      country: faker.address.country(),
+      age: Math.round(Math.random() * 100),
+      _id: new Date().getMilliseconds().toString()
+    }
 
+    this.store.dispatch(personNew({ person }));
   }
 
   update(p: Person) {
